338ab4db7f31baecb6ee0a04076cc727f8cb9a54
Jiradet Amornpimonkul <jiradet.amorn@gmail.com>
2023-05-28 23:48:34 -0400

    Added doc strings and API related file

---
+++ RickAndMorty/APIClients/RMEndpoint.swift
@@ -0,0 +1,18 @@
+//
+//  RMEndpoint.swift
+//  RickAndMorty
+//
+//  Created by Jiradet Amornpimonkul on 5/28/23.
+//
+
+import Foundation
+
+/// Represents unique API endpoint
+@frozen enum RMEndpoint: String {
+    /// Endpoint to get character info
+    case character
+    /// Endpoint to get location info
+    case location
+    /// Endpoint to get episode info
+    case episode
+}
---
+++ RickAndMorty/APIClients/RMRequest.swift
@@ -0,0 +1,13 @@
+//
+//  RMRequest.swift
+//  RickAndMorty
+//
+//  Created by Jiradet Amornpimonkul on 5/28/23.
+//
+
+import Foundation
+
+/// Object that represents a singlet API call
+final class RMRequest {
+    
+}
