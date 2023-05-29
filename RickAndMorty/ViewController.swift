6ea147eb6cdd225a0d5382a3d0553d90200417f9
Jiradet Amornpimonkul <jiradet.amorn@gmail.com>
2023-05-28 23:02:04 -0400

    Initial Commit

---
+++ RickAndMorty/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RickAndMorty
+//
+//  Created by Jiradet Amornpimonkul on 5/28/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
